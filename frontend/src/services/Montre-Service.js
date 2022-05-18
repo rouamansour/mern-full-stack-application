dfd81abfb6acb528028f40674bdb72c0d8d5eec3
rouamansour <rouamansour.rm@gmail.com>
2022-05-18 05:52:53 +0100

    create montre service

---
+++ frontend/src/services/Montre-Service.js
@@ -0,0 +1,27 @@
+import Axios from "../Axios/Api";
+const MONTRE_API = "/montres"
+const fetchMontres = async () => {
+    return await Axios.get(MONTRE_API);
+}
+const fetchMontreById = async (montreID) => {
+    return await Axios.get(MONTRE_API + '/' + montreID);
+}
+const deleteMontre = async (montreID) => {
+    return await Axios.delete(MONTRE_API + '/' + montreID);
+}
+const addMontre = async (montre) => {
+    return await Axios.post(MONTRE_API, montre);
+
+}
+const editMontre = (montre) => {
+    return Axios.put(MONTRE_API + '/' + montre._id, montre);
+
+}
+
+export const MontreService = {
+    fetchMontres,
+    fetchMontreById,
+    deleteMontre,
+    addMontre,
+    editMontre
+} 
