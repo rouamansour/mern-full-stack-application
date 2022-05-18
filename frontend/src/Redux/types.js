b403cdf3e596f56036ce66babf8b469609010056
rouamansour <rouamansour.rm@gmail.com>
2022-05-18 05:58:35 +0100

    contains CRUD method names

---
+++ frontend/src/Redux/types.js
@@ -0,0 +1,6 @@
+// action type
+export const GET_MONTRES = "GET_MONTRES";
+export const DELETE_MONTRE = "DELETE_MONTRE";
+export const ADD_MONTRE = "ADD_MONTRE";
+export const GET_SINGLE_MONTRE = "GET_SINGLE_MONTRE";
+export const UPDATE_MONTRE = "UPDATE_MONTRE";
